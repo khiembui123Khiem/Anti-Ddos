855d26acd0358689a57b7a00d2d2a10591e67d9a
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 21:34:09 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,1 +1,1 @@
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto-rule.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/auto-rule.sh)
