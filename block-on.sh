339c60fc3b3abb7e851396eaa40ed27b264a0a37
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 22:24:42 +0700

    Update block-on.sh

--- block-on.sh
+++ block-on.sh
@@ -1,15 +1,15 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/5818c02087a0422081340424468798b2" \
-     -H "X-Auth-Email: aulax432@gmail.com" \
-     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/56f26a8e48dc86411f384a09a5ca47c8/firewall/rules/274f528ae2c44b7e9b1888b31c6f5d75" \
+     -H "X-Auth-Email: khiemsky4@gmail.com" \
+     -H "X-Auth-Key: e0f9958a15d747f1d7ab339db302b23537292" \
      -H "Content-Type: application/json" \
      --data '{
      "action": "block",
-     "priority": 500,
+     "priority": 1000,
      "paused": false,
      "description": "Block quốc tế",
      "filter": {
-    "id": "cff5e0f781c14510b9d52e2fbdeb5123",
-    "expression": "(not ip.geoip.country in {\"VN\"} and http.user_agent contains \"iPhone\" and http.request.version in {\"HTTP/2\"                    \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"daukute.us\") or (not ip.geoip.country in {\"VN\"} and http.                   user_agent contains \"Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and htt                   p.host eq \"daukute.us\") or (not ip.geoip.country in {\"VN\"} and http.user_agent contains \"Windows\" and http.request.version in {\"HT                   TP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"daukute.us\")",
+    "id": "784782a9cde7475faeb2aa6b5562e605",
+    "expression": "(not ip.geoip.country in {\"KP\" \"VN\" \"KR\"} and http.request.method in {\"GET\" \"POST\"} and http.request.version in {\"HTTP/2\" \"HTTP/3\"})",
     "paused": false,
     "description": "Block quốc tế"
   }
