5932a6e58507850859fadaba05c3bc5764027ac6
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 22:45:40 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 30 ]
+if [ $connect -lt 700 ]
 then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/Captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
