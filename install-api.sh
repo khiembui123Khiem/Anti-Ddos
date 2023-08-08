e4d1f9c41d605d98b42d9f30c13699c4c28f7365
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 21:35:02 +0700

    Update install-api.sh

--- install-api.sh
+++ install-api.sh
@@ -5,6 +5,6 @@ yum install jq -y
 yum install wget -y
 rm -rf status.txt
 rm -rf auto.sh
-wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
-wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
-screen -S api-clf -dm bash auto.sh
+wget https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/status.txt
+wget https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/auto.sh
+screen -S API-Daukute -dm bash auto.sh
