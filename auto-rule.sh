6797a6b4b3d4730a1dbfb994f0f277c329f02a7a
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 21:33:35 +0700

    Update auto-rule.sh

--- auto-rule.sh
+++ auto-rule.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 while true
 do
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/anti.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/anti.sh)
 sleep 2
 done
