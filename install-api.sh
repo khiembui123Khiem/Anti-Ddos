3e079e23eb7bb14973cbd2284e3583ad870b1f2a
khiembui123Khiem <108018525+khiembui123Khiem@users.noreply.github.com>
2023-08-08 22:34:33 +0700

    Update install-api.sh

--- install-api.sh
+++ install-api.sh
@@ -2,7 +2,6 @@ yum install nano -y
 yum install net-tools -y
 yum install screen -y
 yum install jq -y
-yum install wget -y
 rm -rf status.txt
 rm -rf auto.sh
 wget https://raw.githubusercontent.com/khiembui123Khiem/Anti-Ddos/main/status.txt
